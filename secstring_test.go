fdba433222e85e47e05374c0f8aa423905d1aa86
William Orr <will@worrbase.com>
2014-02-23 19:44:49 -0800

    More/better testing of new

--- secstring_test.go
+++ secstring_test.go
@@ -17,6 +17,11 @@ func TestNew(t *testing.T) {
 	for i := 0; i < len(b); i++ {
 		if b[i] != 0 {
 			t.Error("input should have been cleared")
+			t.Errorf("offending byte at offset %v: %v", i, b[i])
 		}
+	}
+
+	if s.Length != 4 {
+		t.Errorf("Expected length 4. Got %v", s.Length)
 	}
 }
